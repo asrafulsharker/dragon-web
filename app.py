e732572d042ea6e451cd981bb60f03bb79733363
Md. Asraful Sharker Nirob <152243736+asrafulsharker-incrementsinc@users.noreply.github.com>
2024-01-06 18:00:21 +0600

    add

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ from keras.preprocessing.image import load_img, img_to_array
 from keras.applications.vgg16 import preprocess_input
 import numpy as np
 import os  
+import tensorflow as tf
+physical_devices = tf.config.list_physical_devices('GPU')
+tf.config.experimental.set_memory_growth(physical_devices[0], True)
+# initialize the Flask application
 
 app = Flask(__name__)
 
